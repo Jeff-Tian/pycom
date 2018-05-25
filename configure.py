7a951ae20f8ebfed79784a93485443d5edb58ff9
Jeff Tian <Jeff.Tian@outlook.com>
2018-05-25 09:14:22 +0800

    show config

--- configure.py
+++ configure.py
@@ -6,8 +6,9 @@ import yaml
 class ConfigureDialog:
     def __init__(self, parent):
         self.modal_window = tk.Toplevel(parent)
-        self.label = tk.Label(self.modal_window, text='实验配置')
-        self.label.pack()
+        # self.label = tk.Label(self.modal_window, text='实验配置')
+        # self.label.pack()
+        self.read_config()
         self.modal_window.grab_set()
 
     def save(self):
@@ -24,4 +25,24 @@ class ConfigureDialog:
         self.update_ui()
 
     def update_ui(self):
-        pass
+        row = 0
+        for key, value in self.config.items():
+            print(key, value, type(value))
+            if type(value) is not list:
+                label = tk.Label(self.modal_window, text=key)
+                label.grid(row=row, column=0, sticky=tk.W)
+
+                the_value = tk.StringVar()
+                the_value.set(value)
+                input_box = tk.Entry(self.modal_window, textvariable=the_value)
+                input_box.grid(row=row, column=1, sticky=tk.W)
+                row += 1
+            else:
+                label = tk.Label(self.modal_window, text=key)
+                label.grid(row=row, column=0, sticky=tk.W)
+                row += 1
+
+                for o in self.config[key]:
+                    label = tk.Label(self.modal_window, text=o['command'])
+                    label.grid(row=row, column=0, sticky=tk.W)
+                    row += 1
