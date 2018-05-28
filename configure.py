f124af7a222fa5288e9d88b2a0384987dc1ce896
Jeff Tian <Jeff.Tian@outlook.com>
2018-05-28 19:03:15 +0800

    make modules UI

--- configure.py
+++ configure.py
@@ -3,6 +3,23 @@ import tkinter as tk
 import yaml
 
 
+def is_stimulate(command):
+    if 'stimulate' in command:
+        if command['stimulate']:
+            return 1
+        else:
+            return 0
+    else:
+        return 0
+
+
+def is_module_checked(command, module_index):
+    if 'module' in command and module_index in command['module'] and command['module'][module_index] is True:
+        return 1
+    else:
+        return 0
+
+
 class ConfigureDialog:
     def __init__(self, parent):
         self.modal_window = tk.Toplevel(parent)
@@ -43,29 +60,41 @@ class ConfigureDialog:
                 row += 1
 
                 for o in self.config[key]:
-                    label = tk.Label(self.modal_window, text=o['command'])
-                    label.grid(row=row, column=0, sticky=tk.W)
+                    self.make_command_name_label(o, row)
                     row += 1
 
-                    o['is_stimulate'] = tk.IntVar(value=self.is_stimulate(o))
-                    chk = tk.Checkbutton(self.modal_window, text='刺激', variable=o['is_stimulate'], onvalue=1,
-                                         offvalue=0)
-                    chk.grid(row=row, column=1, sticky=tk.W)
-
-                    label = tk.Label(self.modal_window, text='触发时间')
-                    label.grid(row=row, column=2, sticky=tk.W)
-
-                    at_text = tk.StringVar(value=o['at'])
-                    text_box = tk.Entry(self.modal_window, width=20, textvariable=at_text)
-                    text_box.grid(row=row, column=3, sticky=tk.W)
+                    self.make_is_stimulate_checkbox(o, row)
+                    self.make_at_field(o, row)
+                    self.make_modules_checkboxes(o, row)
 
                     row += 1
 
-    def is_stimulate(self, command):
-        if 'stimulate' in command:
-            if command['stimulate']:
-                return 1
-            else:
-                return 0
-        else:
-            return 0
+    def make_command_name_label(self, o, row):
+        label = tk.Label(self.modal_window, text=o['command'])
+        label.grid(row=row, column=0, sticky=tk.W)
+
+    def make_at_field(self, o, row):
+        label = tk.Label(self.modal_window, text='触发时间')
+        label.grid(row=row, column=2, sticky=tk.W)
+        at_text = tk.StringVar(value=o['at'])
+        text_box = tk.Entry(self.modal_window, width=20, textvariable=at_text)
+        text_box.grid(row=row, column=3, sticky=tk.W)
+
+    def make_is_stimulate_checkbox(self, command, row):
+        command['is_stimulate'] = tk.IntVar(value=is_stimulate(command))
+        chk = tk.Checkbutton(self.modal_window, text='刺激', variable=command['is_stimulate'], onvalue=1,
+                             offvalue=0)
+        chk.grid(row=row, column=1, sticky=tk.W)
+
+    def make_modules_checkboxes(self, command, row):
+        self.make_module_checkbox(command, 1, row, 5)
+        self.make_module_checkbox(command, 2, row, 6)
+        self.make_module_checkbox(command, 3, row, 7)
+        self.make_module_checkbox(command, 4, row, 8)
+
+    def make_module_checkbox(self, command, module_index, row, col):
+        command['module_' + str(module_index) + '_check'] = tk.IntVar(value=is_module_checked(command, module_index))
+        chk = tk.Checkbutton(self.modal_window, text='模块 ' + str(module_index),
+                             variable=command['module_' + str(module_index) + '_check'],
+                             onvalue=1, offvalue=0)
+        chk.grid(row=row, column=col, sticky=tk.W)
